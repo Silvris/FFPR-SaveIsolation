b9ceb24957011c54643af86998fed17778d260d0
Silvris <58583688+Silvris@users.noreply.github.com>
2021-10-03 21:52:53 -0500

    Initial (working) commit

---
+++ FFPR-SaveIsolation/EntryPoint.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnhollowerRuntimeLib;
+
+namespace FFPR_SaveIsolation
+{
+    [BepInPlugin("silvris.ffpr.save_isolation", "Save Isolation", "1.0.0.0")]
+    [BepInProcess("FINAL FANTASY.exe")]
+    [BepInProcess("FINAL FANTASY II.exe")]
+    [BepInProcess("FINAL FANTASY III.exe")]
+    [BepInProcess("FINAL FANTASY IV.exe")]
+    [BepInProcess("FINAL FANTASY V.exe")]
+    [BepInProcess("FINAL FANTASY VI.exe")]
+    public class EntryPoint : BasePlugin
+    {
+        public static EntryPoint Instance { get; private set; }
+        public override void Load()
+        {
+            Instance = this;
+            Log.LogInfo("Loading...");
+            ClassInjector.RegisterTypeInIl2Cpp<SaveSlotManager_GetSlotPath>();
+            PatchMethods();
+        }
+        private void PatchMethods()
+        {
+            try
+            {
+                Log.LogInfo("Patching methods...");
+                Harmony harmony = new Harmony("silvris.ffpr.save_isolation");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to patch methods.", ex);
+            }
+        }
+    }
+}
---
+++ FFPR-SaveIsolation/SaveSlotManager_GetSlotPath.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Last.Management;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFPR_SaveIsolation
+{
+    [HarmonyPatch(typeof(SaveSlotManager),nameof(SaveSlotManager.GetSavePath))]
+    public sealed class SaveSlotManager_GetSlotPath : Il2CppSystem.Object
+    {
+        public SaveSlotManager_GetSlotPath(IntPtr ptr) : base(ptr)
+        {
+
+        }
+        public static void Postfix(SaveSlotManager __instance, ref string __result)
+        {
+            EntryPoint.Instance.Log.LogInfo($"Changing path \"{__result} to {__result + "Isolated/"}");
+            __result += "Isolated/";
+        }
+    }
+}
